cbc5e6ea9828c2cc55aaa66686e4ad9dda1ba471
jdelarcopdam1b <91684541+jdelarcopdam1b@users.noreply.github.com>
2022-01-27 23:52:14 +0100

    fix

--- Test.java
+++ Test.java
@@ -9,16 +9,16 @@ public class Test {
         int faltasNoJustificadas;
         int faltasJustificadas;
 
-        String lineaDatos = " Diego nikolay , hristozov pardo,13, 8";
+        String lineaDatos = "Catarina    rafaela , Layos Almeida , 45, 23";
 
-        String[] clean = lineaDatos.trim().split(",");
+        String[] clean = lineaDatos.trim().split(", {1,}");
 
         nombre = clean[0].trim();
         apellidos = clean[1].trim().toUpperCase();
         faltasNoJustificadas = Integer.parseInt(clean[2]);
         faltasJustificadas = Integer.parseInt(clean[3]);
 
-        System.out.println(nombre);
+        System.out.println(nombre.replaceAll("\\s{2,}"," ").trim());
         System.out.println(apellidos);
         System.out.println(faltasNoJustificadas);
         System.out.println(faltasJustificadas);
