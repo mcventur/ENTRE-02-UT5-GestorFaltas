659fbcca1d8858f8297ca405e6b883fcadb15a33
jdelarcopdam1b <91684541+jdelarcopdam1b@users.noreply.github.com>
2022-01-28 13:48:25 +0100

    fin fix

--- Test.java
+++ Test.java
@@ -7,7 +7,7 @@ public class Test {
         int faltasNoJustificadas;
         int faltasJustificadas;
 
-        String lineaDatos = " pedro josé   andrés  ,  Troya Baztarrica , 42, 6 ";
+        String lineaDatos = "  Asier  Elorri, Oyarzun Guallar,  45, 7";
 
         String[] clean = lineaDatos.trim().split(",");
 
@@ -19,12 +19,15 @@ public class Test {
         String temp2nombre = nombre.replaceAll("\\s{2,}"," "); //Limpio los espacios entre las palabras
         String[] complejo = temp2nombre.split(" "); // Las divido con un split para manipularlas de forma independiente
 
+        String str = "";
         for(int i=0; i<complejo.length-1; i++){ // Solo busca los dos primeros nombres menos el último
-            System.out.print(complejo[i].substring(0,1).toUpperCase() + "."); // Para mostrar solo las iniciales con el punto
+            str += complejo[i].substring(0,1).toUpperCase() + "."; // Para mostrar solo las iniciales con el punto
         }
 
-        //System.out.println(nombre);
-        System.out.println("\n" + apellidos);
+        nombre = str + complejo[complejo.length-1].replace(complejo[complejo.length-1].charAt(0), complejo[complejo.length-1].toUpperCase().charAt(0));
+
+        System.out.println(nombre);
+        System.out.println(apellidos);
         System.out.println(faltasNoJustificadas);
         System.out.println(faltasJustificadas);
     }
